c55aaa6602b57b2ea3b292acc242c8b5e01c4fe1
Cordero Sanchez <laura-nathalia.cordero-sanchez@capgemini.com>
2024-10-18 11:00:20 +0200

    Despliegue Eureka

---
+++ EurekaServer/src/test/java/com/capgeticket/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgeticket.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
